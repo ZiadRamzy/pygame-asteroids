d7b5c77265a31c08e29b67ee92d9c9679a246f4a
Ziad <z.abdelfatah@treesense.net>
2025-11-04 15:51:20 +0100

    include game state strings and constants

---
+++ constants.py
@@ -0,0 +1,16 @@
+# screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# colors (R,G,B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (100, 100, 100)
+
+# game title
+GAME_TITLE = "ASTEROIDS CLONE"
+
+# game states
+GAME_STATE_MENU  = "MENU"
+GAME_STATE_PLAYING = "PLAYING"
